d675ebc3eda00c3f6aec2950ae56d759a091ec3a
gakiokevin <gakiokevin5@gmail.com>
2023-09-22 19:29:13 +0300

    ready for production

--- frontend/src/Welcome.jsx
+++ frontend/src/Welcome.jsx
@@ -1,20 +1,9 @@
 
- const style = {
-   backgroundColor:'lightgray',
-   maxWidth:'1000px',
-   height:'100px',
-   margin:'30px auto',
-   padding:'10px',
-   borderRadius:'3px'
-
- }
-
-
 const Welcome = ()=>{
    return (
-      <div style={style} className="welcome">
-         <h2 style={{margin:'3px 4px'}} className="welcomeTitle">ImagesGallery</h2>
-         <p style={{marginTop:'12px',fontSize:'16px'}} className="welcometext">Welcome to ImagesGallery, this is a simple web application the fetches images from UNSPLASH API.To get started, search any image on the input field above</p>
+      <div className="welcome">
+         <h2  className="welcomeTitle">ImagesGallery</h2>
+         <p  className="welcometext">Welcome to ImagesGallery, this is a simple web application the fetches images from UNSPLASH API.To get started, search any image on the input field above</p>
       </div>
    )
 }
